b19be8487d96eb963b988b00e1614ff76a79f7b6
Wallace Santana <wallacesantanak0@gmail.com>
2024-11-09 16:23:54 -0300

    update

--- api/index.php
+++ api/index.php
@@ -1,2 +1,16 @@
 <?php
-phpinfo();
+    header('Content-Type: application/json');
+    $requestMethod = $_SERVER['REQUEST_METHOD'];
+    $requestUri = $_SERVER['REQUEST_URI'];
+    
+    switch ($requestMethod) {
+        case 'GET':
+            if (strpos($requestUri, '/') !== false) {
+               echo 'está rodando';
+            }
+            break;
+        default:
+            echo json_encode(['message' => 'Método não suportado']);
+            break;
+    }
+?>
