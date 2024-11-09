f96bc2ac9c5542ecf1cb33d0962ddb4406a815ae
Wallace Santana <wallacesantanak0@gmail.com>
2024-11-09 16:17:25 -0300

    funcionando

--- api/index.php
+++ api/index.php
@@ -1,16 +1,3 @@
 <?php
-    header('Content-Type: application/json');
-    $requestMethod = $_SERVER['REQUEST_METHOD'];
-    $requestUri = $_SERVER['REQUEST_URI'];
-    
-    switch ($requestMethod) {
-        case 'GET':
-            if (strpos($requestUri, '/') !== false) {
-               echo 'está rodando';
-            }
-            break;
-        default:
-            echo json_encode(['message' => 'Método não suportado']);
-            break;
-    }
+ echo "Hello World";
 ?>
