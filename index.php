7ea9b8d3902b07c54313125e13f806b6dff6ccfd
Wallace Santana <wallacesantanak0@gmail.com>
2024-11-12 00:20:41 -0300

    agora vai

--- index.php
+++ index.php
@@ -1,17 +1,13 @@
 <?php
-// index.php na raiz
 
-// Remove parâmetros de consulta e barras finais da URL para comparação precisa
 $request_uri = strtok($_SERVER['REQUEST_URI'], '?');
 $request_uri = rtrim($request_uri, '/');
 
-// Verifica a URL e chama o script correspondente
 if ($request_uri == '/login') {
     include 'api/login.php';
-} elseif ($request_uri == '/cadastro') {
+} elseif ($request_uri == '/register') {
     include 'api/cadastro.php';
 } else {
-    // Rota padrão ou 404
     http_response_code(404);
     echo "Página não encontrada.";
 }
