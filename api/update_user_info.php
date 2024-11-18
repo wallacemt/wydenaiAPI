0bcccae5cbbc9183b464d3c7e0284719b1a2543d
Wallace Santana <wallacesantanak0@gmail.com>
2024-11-17 22:53:40 -0300

    att CORS

--- api/update_user_info.php
+++ api/update_user_info.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: http://localhost:5173");
+header("Access-Control-Allow-Origin: https://wydenai-client.vercel.app/");
 header("Access-Control-Allow-Methods: GET, POST, PUT, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
