6466032e941514be1f74af26a276abaacf918006
Wallace Santana <wallacesantanak0@gmail.com>
2024-11-17 22:58:28 -0300

    CORS

--- api/delete_user.php
+++ api/delete_user.php
@@ -1,5 +1,5 @@
 <?php
-header("Access-Control-Allow-Origin: https://wydenai-client.vercel.app/");
+header("Access-Control-Allow-Origin: https://wydenai-client.vercel.app");
 header("Access-Control-Allow-Methods: GET, POST, PUT, DELETE, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 header("Access-Control-Allow-Credentials: true");
