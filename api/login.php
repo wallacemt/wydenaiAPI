fe1969c19d2b677c2690fd174502296597bc9f6e
Wallace Santana <138271229+wallacemt@users.noreply.github.com>
2024-11-17 21:30:42 -0300

    Update login.php

--- api/login.php
+++ api/login.php
@@ -5,16 +5,23 @@ header("Access-Control-Allow-Methods: POST, GET, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type, Authorization");
 
 
-require_once __DIR__ . '/../vendor/autoload.php';  // Caminho absoluto baseado na localização do script
+// require_once __DIR__ . '/../vendor/autoload.php';  // Caminho absoluto baseado na localização do script
 
 
-$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
-$dotenv->load();
+// $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . '/../');
+// $dotenv->load();
 
-$host = $_ENV['DB_HOST'];
-$dbname = $_ENV['DB_NAME'];
-$username = $_ENV['DB_USER'];
-$password = $_ENV['DB_PASSWORD'];
+// $host = $_ENV['DB_HOST'];
+// $dbname = $_ENV['DB_NAME'];
+// $username = $_ENV['DB_USER'];
+// $password = $_ENV['DB_PASSWORD'];
+
+$host = getenv('DB_HOST');
+$dbname = getenv('DB_NAME');
+$username = getenv('DB_USER');
+$password = getenv('DB_PASSWORD');
+
+
 
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname", $username, $password);
